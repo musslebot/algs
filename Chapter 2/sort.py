6bc76a4eb598cccb5cc76db0be2265df6f2e00e9
musslebot <mdmusslewhite@gmail.com>
2016-07-24 12:13:33 -0500

    reverse insertion sort

--- Chapter 2/sort.py
+++ Chapter 2/sort.py
@@ -12,3 +12,18 @@ def insertion_sort(sequence):
             i -= 1
         sequence[i+1] = key
     return sequence
+
+
+def reverse_insertion_sort(sequence):
+    """ Return the sequence with elements descending.
+
+        In-place reverse insertion sort.
+    """
+    for j in range(1, len(sequence)):
+        key = sequence[j]
+        i = j - 1
+        while i >= 0 and key > sequence[i]:
+            sequence[i+1] = sequence[i]
+            i -= 1
+        sequence[i+1] = key
+    return sequence
