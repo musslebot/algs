d4dc16da55da5088dd23138274839fe0f90d54a5
musslebot <mdmusslewhite@gmail.com>
2016-07-24 12:12:51 -0500

    insertion sort

---
+++ Chapter 2/sort.py
@@ -0,0 +1,14 @@
+
+def insertion_sort(sequence):
+    """ Return the sequence with elements ascending.
+
+        In-place insertion sort.
+    """
+    for j in range(1, len(sequence)):
+        key = sequence[j]
+        i = j - 1
+        while i >= 0 and key < sequence[i]:
+            sequence[i+1] = sequence[i]
+            i -= 1
+        sequence[i+1] = key
+    return sequence
